a622140ad1c137cb229508c29b80714ee9ca1cde
todd-eagle <todd.eagle@mail.strayer.edu>
2020-05-13 12:05:12 -0400

    Committing changes

--- classes.js
+++ classes.js
@@ -31,6 +31,20 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+
+}
+
 
 ////////// PROBLEM 2 //////////
 
@@ -49,12 +63,28 @@
 
 //Code Here
 
+class Manager extends Employee {
+
+  constructor(first_name, last_name, email, age){
+      super(first_name, last_name, email, age)
+      this.reports = []
+  }
+  hire(employee){
+      this.reports.push(employee)
+      return this.reports
+  }
+  fire(index){
+      this.reports.splice(index, 1)
+      return this.reports
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
 /*
   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the following additional properties:
+  create a class ProgressiveManager that extends Manager.  A Progressive Manager has all of the same properties as a manager with the 
+  following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
 
@@ -72,7 +102,39 @@
 */
 
 //Code Here
+class ProgressiveManager extends Manager{
 
+  constructor(first_name, last_name, email, age){
+      super(first_name, last_name, email, age)
+      this.bonus = 0
+      this.title = 'Not a manager'
+  }
+
+
+  hire(){
+      super.hire()
+      let count = this.reports.length;
+      this.changeTitle(count)
+  }
+
+  fire(index){
+      if(super.fire(index)){
+         // this.reportCount(this.reports)
+         this.bonus +=100
+         let count = this.reports.length;
+      }
+  }
+  
+  changeTitle(count){
+     
+    if(count > 0 && count < 4){this.title = 'Barely Manager'}
+    else if(count > 3  && count < 11) {this.title = 'Mostly Manager'}
+    else if(count > 10  && count < 51) {this.title ='Manager'}
+    else if(count > 50  && count < 101) {this.title = 'Manager Plus'}
+    else{ this.title =  'Bestest Manager'}
+  }
+
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -100,4 +162,29 @@
 
 //Code Here
 
+class Machine {
+ constructor(){
+   this.widgets_made_count = 0
+   this.wear_and_tear_count = 0
+   this.needs_reboot = false
+ }
+
+ makeWidgets(num){
+  this.widgets_made_count += num
+  this.wear_and_tear_count = Math.floor( this.widgets_made_count / 50)
+ }
+
+ fixMachine(){
+  this.needs_reboot = true
+ }
+
+ reboot(){
+   return () =>{ 
+      this.wear_and_tear_count -=10
+      this.needs_reboot = false
+    } 
+
+ }
+   
+}
 
